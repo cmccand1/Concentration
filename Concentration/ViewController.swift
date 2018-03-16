2de08e2ed2ffd8d5611684458ba0b51668ebfbd0
cmccand1 <clint.mccandless@gmail.com>
2018-03-16 09:44:16 -0600

    Lecture 1

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // Instance variable
+    var flipCount: Int = 0 {
+        // didSet is executed every time flipCount is changed somewhere else in the code
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Instance variable which points to the UILabel
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    // Instance variable of type Array<UIButton>
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    // Array<String> holds the card titles
+    var emojiChoices = ["🎃", "👻", "🎃", " 👻"]
+    
+    // Method/function executed every time a connected card is touched
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("The chosen card is not in cardButtons")
+        }
+    }
+    
+    // Method/function which specifies the action taken when a card is touched
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
     }
 
-
 }
-
