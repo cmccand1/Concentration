160a2763f3ddf6e12fdbd637e4696a4696e905eb
cmccand1 <clint.mccandless@gmail.com>
2018-03-16 15:36:26 -0600

    Fixed flipCount to only increment on the initial flip of a card.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -29,7 +29,6 @@ class ViewController: UIViewController {
     
     // Method/function executed every time a connected card is touched
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
             flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
         } else {
@@ -43,6 +42,7 @@ class ViewController: UIViewController {
             button.setTitle("", for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
+            flipCount += 1
             button.setTitle(emoji, for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
